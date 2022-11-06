0b3321e199c13a77d7bc26127414487831352d6a
NeoGendaijin <aming4mit@gmail.com>
2022-11-06 10:13:52 +0900

    modifiled main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ parser.add_argument('--save_dir', type=str, default='Images')
 args = parser.parse_args()
 
 def main(args):
-    print('a')
-    pymupdf = PyMuPDF(args.pdf_file)
-    images = pymupdf.crop_imgs_from_pdf()
-    pymupdf.save_images(images, args.save_dir)
+     #take from all pdf files
+    for pdf_fl in glob.glob(args.pdf_file):
+        pymupdf = PyMuPDF(pdf_fl)
+        images = pymupdf.crop_imgs_from_pdf()
+        pymupdf.save_images(images, args.save_dir)
 
 if __name__ == '__main__':
     main(args)
