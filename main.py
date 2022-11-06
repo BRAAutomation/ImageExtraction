e3ea5b90f768b1497aa2fbdeae5d25cfd3c50b38
NeoGendaijin <aming4mit@gmail.com>
2022-11-06 10:27:39 +0900

    first commit

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import argparse
 from util import *
+import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--pdf_file', type=str, default='PDF/sample.pdf')
+parser.add_argument('--pdf_file', type=str, default='PDF/*')
 parser.add_argument('--save_dir', type=str, default='Images')
 args = parser.parse_args()
 
 def main(args):
-     #take from all pdf files
+    #take from all pdf files
     for pdf_fl in glob.glob(args.pdf_file):
         pymupdf = PyMuPDF(pdf_fl)
         images = pymupdf.crop_imgs_from_pdf()
