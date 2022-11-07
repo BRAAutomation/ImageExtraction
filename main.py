89032be00f79a83436c5dfc30d018f6aed84d345
NeoGendaijin <aming4mit@gmail.com>
2022-11-07 15:43:43 +0900

    modified main.py and util.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 import argparse
 from util import *
+import glob
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--pdf_file', type=str, default='PDF/sample.pdf')
+parser.add_argument('--pdf_file', type=str, default='PDF/*')
 parser.add_argument('--save_dir', type=str, default='Images')
 args = parser.parse_args()
 
 def main(args):
-    print('a')
-    pymupdf = PyMuPDF(args.pdf_file)
-    images = pymupdf.crop_imgs_from_pdf()
-    pymupdf.save_images(images, args.save_dir)
+    #take from all pdf files
+    for pdfNo, pdf_fl in enumerate( glob.glob(args.pdf_file) ):
+        pymupdf = PyMuPDF(pdf_fl)
+        images = pymupdf.crop_imgs_from_pdf()
+        pymupdf.save_images(pdfNo, images, args.save_dir)
 
 if __name__ == '__main__':
     main(args)
--- util.py
+++ util.py
@@ -2,31 +2,41 @@
 
 # PyMuPDFをインポート（歴史的な経緯でなぜかfitzのインポートになっていることに注意）
 import fitz
+import os
 # import fitz as pymu とかしておくと読み込み時にPyMuPDF感が出るかもしれない
 
 class PyMuPDF():
     def __init__(self, filename):
         self.filename = filename
 
-    def save_images(self, images, save_dir):
+    def save_images(self, pdfNo, images, save_dir):
         pdf_file = fitz.open(self.filename)
-        counter = 0
-        #images内のデータをcounterに合わせて保存
-        if not len(images) == 0:
-            for image in images:
-                counter += 1
-                xref = image[0]
-                img = pdf_file.extract_image(xref)
-                with open(save_dir + "/extracted_image{}.png".format(counter), "wb") as f:
-                    f.write(img["image"])
+        #保存ディレクトリが指定されていない時は作る
+        if os.path.isdir(save_dir) == False:
+            os.mkdir(save_dir)
+        for pageNo, image in enumerate(images):
+            if image != []:
+                for i in range(len(image)):
+                    xref = image[i][0]
+                    #smask = image[i][1]
+                    if image[i][8] == 'FlateDecode':
+                        ext = 'png'
+                    elif image[i][8] == 'DCTDecode':
+                        ext = 'jpeg'
+
+                    pix = fitz.Pixmap(pdf_file.extract_image(xref)["image"])
+                    if not pix.colorspace.name in (fitz.csGRAY.name, fitz.csRGB.name):
+                        pix = fitz.Pixmap(fitz.csRGB, pix)
+                        pix.invert_irect(pix)
+
+                    img_name = os.path.join(save_dir, f'image{pdfNo+1}_{pageNo+1}_{i}.{ext}')
+                    pix.save(img_name)
 
     # return : list of images
     def crop_imgs_from_pdf(self):
         pdf_file = fitz.open(self.filename)
         image_list = []
-        # pdf から画像を抜き取る
-        for page in pdf_file:
-           images = page.get_images()
-           for image in images:
-                image_list.append(image)
+        # pdf から画像を抜き取りリストにして返す
+        for page in range(len(pdf_file)):
+            image_list.append(pdf_file[page].get_images())
         return image_list
